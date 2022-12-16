6fe8ebf73b422f402f26c7d331e122631915c5d9
Nicolò Curioni <nicolo.curioni96@gmail.com>
2022-12-16 09:16:39 +0100

    Initial Commit

---
+++ AR Place/AR_PlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  AR_PlaceApp.swift
+//  AR Place
+//
+//  Created by Nicolò Curioni on 16/12/22.
+//
+
+import SwiftUI
+
+@main
+struct AR_PlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
